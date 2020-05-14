4a506c425fd67f2bef45b8648e1504f31c8cbe64
CopaceticVT <34892703+CopaceticVT@users.noreply.github.com>
2020-05-13 21:43:29 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+cd && pwd
+echo "SCRIPT [Setting up server with Ansible]"
+echo "TASK [Install PPA]"
+sudo apt-get install software-properties-common -y
+
+echo "TASK [Add Ansible Repo]"
+sudo apt-add-repository ppa:ansible/ansible -y
+
+echo "TASK [Update & Upgrade]"
+sudo apt update -y && sudo apt upgrade -y
+
+echo "TASK [Install Ansbile]"
+sudo apt install ansible -y
+
+echo "TASK [Create raz_playbooks Directory]"
+cd && pwd && mkdir "raz_playbooks" && cd "raz_playbooks" && pwd
+
+echo "TASK [Git Glone]"
+git clone https://github.com/CopaceticVT/aws-ubuntu-lamp-wordpress.git
+
+echo "TASK [Change to Project Directory]"
+cd aws-ubuntu-lamp-wordpress
+
+echo "TASK [Run Playbook]"
+sudo ansible-playbook raz-setup.yml -e 'ansible_python_interpreter=/usr/bin/python3'
